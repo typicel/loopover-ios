b0416f26ee979465ecc7dec592d88a9fe8607ef5
Noah Tabori <nota5633@colorado.edu>
2024-03-02 14:30:18 -0700

    spacing - Vgrid

--- Loopover/ContentView.swift
+++ Loopover/ContentView.swift
@@ -42,16 +42,16 @@ struct ContentView: View {
         VStack {
             Spacer()
             Text(formatTime(self.board.seconds))
-            LazyVGrid(columns: Array(repeating: GridItem(), count: board.cols), spacing: 10) {
-                ForEach(0..<board.rows * board.cols, id: \.self) { index in
-                    Text("\(board.board[index / board.cols][index % board.cols])")
-                        .font(.title)
-                        .bold()
-                        .frame(width: 50, height: 50)
-                        .border(Color.white)
-                        .foregroundColor(.white)
-                }
-            }
+            LazyVGrid(columns: Array(repeating: GridItem(), count: board.cols), spacing: 0) {
+                        ForEach(0..<board.rows * board.cols, id: \.self) { index in
+                            Text("\(board.board[index / board.cols][index % board.cols])")
+                                .font(.title)
+                                .bold()
+                                .frame(width: 75, height: 75)
+                                .border(Color.white)
+                                .foregroundColor(.white)
+                        }
+                    }
             Spacer()
             Button(action: {board.scramble(); board.startTimer()}) {
                 Image(systemName: "arrow.triangle.2.circlepath")
@@ -66,6 +66,7 @@ struct ContentView: View {
             self.board.startTimer()
         }
         .padding()
+        .frame(maxWidth: .infinity)
         .background(Color(cTeal))
     }
     
