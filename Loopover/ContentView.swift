599852d1fec003b09dfac19d5fe4fc9eb913ecb6
typicel <tyler.mccormick@colorado.edu>
2024-03-02 10:30:19 -0700

    Initial Commit

---
+++ Loopover/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Loopover
+//
+//  Created by Tyler McCormick on 3/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Loopover/LoopoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopoverApp.swift
+//  Loopover
+//
+//  Created by Tyler McCormick on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LoopoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
