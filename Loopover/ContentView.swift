fdbb7077ef515b5cbbef2afa0816300f3842c6f0
Noah Tabori <nota5633@colorado.edu>
2024-03-02 13:50:26 -0700

    UIColor Extension made, initial teal design

--- Loopover/ContentView.swift
+++ Loopover/ContentView.swift
@@ -7,26 +7,52 @@
 
 import SwiftUI
 
+extension UIColor {
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
+let cTeal = Color(UIColor(hex: "#007173"))
+
 struct ContentView: View {
     @ObservedObject var board = Board(5,5)
     var body: some View {
         VStack {
-
+            Spacer()
             LazyVGrid(columns: Array(repeating: GridItem(), count: board.cols), spacing: 10) {
                 ForEach(0..<board.rows * board.cols, id: \.self) { index in
                     Text("\(board.board[index / board.cols][index % board.cols])")
                         .frame(width: 50, height: 50)
-                        .border(Color.black)
+                        .border(Color.white)
+                        .foregroundColor(.white)
                 }
             }
-            
-            Button("Scramble") {
-                board.scramble()
+            Spacer()
+            Button(action: {board.scramble()}) {
+                Image(systemName: "arrow.triangle.2.circlepath")
+                    .padding()
+                    .font(.title)
+                    .foregroundColor(.teal)
             }
+            .background(Color.white)
+            .cornerRadius(10)
         }
         .padding()
+        .background(Color(cTeal))
     }
 }
+
 
 #Preview {
     ContentView()
