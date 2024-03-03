2980d1d453394f045678fd49c62ab5245dc862ad
typicel <tyler.mccormick@colorado.edu>
2024-03-02 23:48:55 -0700

    add bg color

--- Loopover/Box.swift
+++ Loopover/Box.swift
@@ -10,17 +10,18 @@ import SwiftUI
 struct Box: View {
     var text: String
     var size: CGFloat
+    var color: Color
     
     var body: some View {
         Text(text)
             .font(.title)
             .bold()
             .frame(width: size, height: size)
-            .border(Color.white)
-            .foregroundColor(.white)
+            .foregroundStyle(Color.black)
+            .background(color)
     }
 }
 
 #Preview {
-    Box(text: "A", size: 75)
+    Box(text: "A", size: 75, color: Color(red: 1.0, green: 0.0, blue: 0.0))
 }
